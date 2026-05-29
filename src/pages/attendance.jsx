5a53cd7c3324c7cfc6e3444582e03e19c2cd8b3a
Sneha Bagal <b09sneha@gmail.com>
2026-05-29 14:23:51 +0530

    Fix remaining API env URLs

--- src/pages/attendance.jsx
+++ src/pages/attendance.jsx
@@ -23,7 +23,7 @@ export default function Attendance() {
       const token = localStorage.getItem("token");
 
 const res = await axios.get(
-  "`${import.meta.env.VITE_API_URL}/attendance",
+  `${import.meta.env.VITE_API_URL}/attendance",
   {
     headers: {
       Authorization: `Bearer ${token}`
