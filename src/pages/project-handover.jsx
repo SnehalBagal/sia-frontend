935aa5f7d0954c0ae03f1cb65c4d6244756e9c42
Sneha Bagal <b09sneha@gmail.com>
2026-08-04 15:57:50 +0530

    Added Project Handover records list

--- src/pages/project-handover.jsx
+++ src/pages/project-handover.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import Sidebar from "../components/Sidebar";
+import ProjectHandoverList from "./ProjectHandoverList";
 
 export default function ProjectHandover() {
 
@@ -441,12 +442,14 @@ export default function ProjectHandover() {
 
           </div>
 
-          </div>
+            </div>
 
+          {/* Project Handover Records */}
+          <ProjectHandoverList />
 
       </div>
 
-</div>
+    </div>
 
 );
-}
+} 
